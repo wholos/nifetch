7249153369b4ecdc6d5a3f7a21ff47987b4f3bd2
holos <holosnikitatt@gmail.com>
2025-07-22 16:43:43 +0300

    Create wm.nim

---
+++ wm.nim
@@ -0,0 +1,17 @@
+import osproc, strutils
+
+const knownWMs = [
+  "i3", "sway", "bspwm", "openbox", "fluxbox", "xmonad", "awesome", "icewm",
+  "dwm", "herbstluftwm", "spectrwm", "pekwm", "compiz", "metacity", "kwin",
+  "mutter", "xfwm4", "enlightenment", "river", "cwm", "twm"
+]
+
+proc detectWM(): string =
+  let (psOut, _) = execCmdEx("ps -e")
+  for wm in knownWMs:
+    if psOut.toLowerAscii.contains(wm):
+      return wm
+  return "Unable to determine wm"
+
+proc echowm*() =
+   echo "WM: ", detectWM()
