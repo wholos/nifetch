2ac1e13d3f5d4b91e8755e405cc2a376c81f5dc2
holos <holosnikitatt@gmail.com>
2025-07-26 21:38:17 +0300

    Update main.nim

--- main.nim
+++ main.nim
@@ -1,4 +1,4 @@
-import os, osproc, strutils, sysinfo, wm
+import os, osproc, strutils, sysinfo
 
 for arg in commandLineParams():
     case arg
@@ -9,6 +9,23 @@ for arg in commandLineParams():
             echo "Unknown arguments: ", arg
             quit(0)
 
+
+const knownWMs = [
+  "i3", "sway", "bspwm", "openbox", "fluxbox", "xmonad", "awesome", "icewm",
+  "dwm", "herbstluftwm", "spectrwm", "pekwm", "compiz", "metacity", "kwin",
+  "mutter", "xfwm4", "enlightenment", "river", "cwm", "twm"
+]
+
+proc detectWM(): string =
+  let (psOut, _) = execCmdEx("ps -e")
+  for wm in knownWMs:
+    if psOut.toLowerAscii.contains(wm):
+      return wm
+  return "Unable to determine wm"
+
+proc echowm() =
+   echo "WM: ", detectWM()
+            
 proc echode() =
    if execProcess("echo $XDG_CURRENT_DESKTOP").strip().len > 0:
        echo "DE: ", execProcess("echo $XDG_CURRENT_DESKTOP").strip()
